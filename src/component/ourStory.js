f04584e5aa70a50b649d3593b16667a092cfd078
kumar <kumarvermaankit9555@gmail.com>
2022-01-21 20:55:09 +0530

    final

--- src/component/ourStory.js
+++ src/component/ourStory.js
@@ -2,28 +2,28 @@ import React from 'react';
 
 export default function OurStory() {
     return (
-        <div style={{ width: "75%", marginLeft: "13.7%" }}>
-            <p>
+        <div style={{ width: "75%", marginLeft: "13.7%", marginTop: "2%", fontFamily: "Mochiy Pop P One, sans-serif" }}>
+            <p style={{ fontFamily: "Mochiy Pop P One, sans-serif" }}>
                 We at Studio Bodhi believe ‘Design’ is a medium which could be used as a trigger in defining the future of Our collective civilization. Studio Bodhi through its long term vision and motto of “Design for All” celebrates the rich knowledge of our land of building communities, culture, art and crafts, built heritage thus connecting the dots of tangible and intangible.
 
             </p>
-            <p>
+            <p style={{ fontFamily: "Mochiy Pop P One, sans-serif" }}>
                 Our practice associates with the process of exchange of ideas, stories, emotions, flora & fauna, thus weaving & nurturing spaces which celebrates the energy of inclusiveness. Often, these vocabulary of design ideas leads to creating a culture, which defines values and determining the future.
 
             </p>
-            <p>
+            <p style={{ fontFamily: "Mochiy Pop P One, sans-serif" }}>
                 Studio Bodhi acts as melting pot of ideas and experiences which bridges between rural and urban, traditional and contemporary, past and future . . .
 
             </p>
-            <p>
+            <p style={{ fontFamily: "Mochiy Pop P One, sans-serif" }}>
                 Bodhi is a collaboration of designers, architects, engineers, artists, craftsmen, sustainable experts, management professionals, and all likeminded individuals who come together with a shared vision for the betterment of the built environment, thus preserving and leading the future.
 
             </p>
-            <p>
+            <p style={{ fontFamily: "Mochiy Pop P One, sans-serif" }}>
                 Founded in 2018, with works ranging from Urban Design to Product Design, from Architecture to Furniture Design, Interior to Installation, Studio Bodhi works on various scale which helps in bridging the gap between design and all allied expertise under one brand.
 
             </p>
-            <p>
+            <p style={{ fontFamily: "Mochiy Pop P One, sans-serif" }}>
                 We love to Design.
                 <br />
                 For Urban transformation,

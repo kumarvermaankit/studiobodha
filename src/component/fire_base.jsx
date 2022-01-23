993db3e82fd7e136d5ea988cd734ca859b400e41
kumar <kumarvermaankit9555@gmail.com>
2022-01-23 20:34:49 +0530

    final

--- src/component/fire_base.jsx
+++ src/component/fire_base.jsx
@@ -3,19 +3,32 @@ import "firebase/storage"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3ZZs_J-WXJR2-0s-V_XS-cL98PSPQChI",
-  authDomain: "cyduck-3be89.firebaseapp.com",
-  databaseURL: "https://cyduck-3be89.firebaseio.com",
-  projectId: "cyduck-3be89",
-  storageBucket: "cyduck-3be89.appspot.com",
-  messagingSenderId: "809270325989",
-  appId: "1:809270325989:web:0a0883d611ced9086f1a0c",
-  measurementId: "G-EBYN4PJV24"
+  apiKey: "AIzaSyDZ297nrLnK78Ujhg4cHaN2wIey30IFLFs",
+  authDomain: "studiobodhi-a1bf4.firebaseapp.com",
+  projectId: "studiobodhi-a1bf4",
+  storageBucket: "studiobodhi-a1bf4.appspot.com",
+  messagingSenderId: "545185000173",
+  appId: "1:545185000173:web:8a7cece9f93997e0a2bae4",
+  measurementId: "G-QCETGWVEK1"
 };
-
 
 firebase.initializeApp(firebaseConfig)
 
 const storage = firebase.storage();
 
 export { storage, firebase as default }
+
+
+// // // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
